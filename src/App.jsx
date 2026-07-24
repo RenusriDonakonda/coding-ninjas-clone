f4179507650cc410460bef63ec91ec0c32d5ecdd
RenusriDonakonda <renusridonakonda25@gmail.com>
2026-07-24 18:40:55 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 
 import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import Reviews from "./pages/Reviews";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
-
-function App() {
-
+export default function App() {
   return (
-
     <Routes>
-
       <Route element={<MainLayout />}>
+        <Route index element={<Home />} />
 
-        <Route 
-          path="/" 
-          element={<Home />} 
-        />
+        <Route path="courses" element={<Courses />} />
 
+        <Route path="reviews" element={<Reviews />} />
+
+        <Route path="contact" element={<Contact />} />
+
+        <Route path="*" element={<NotFound />} />
       </Route>
-
     </Routes>
-
   );
-
 }
-
-
-export default App;
