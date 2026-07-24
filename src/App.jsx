fc5b136596225e7e362e76f653958c7f59e1e087
RenusriDonakonda <renusridonakonda25@gmail.com>
2026-07-24 17:26:50 +0530

    Refactor App component to use react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+
 
 function App() {
-  return <AppRoutes />;
+
+  return (
+
+    <Routes>
+
+      <Route element={<MainLayout />}>
+
+        <Route 
+          path="/" 
+          element={<Home />} 
+        />
+
+      </Route>
+
+    </Routes>
+
+  );
+
 }
+
 
 export default App;
